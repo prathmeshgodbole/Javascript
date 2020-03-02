586895a3b8edde892b9c21640de2e3a502771924
prathmeshgodbole <godbole611@gmail.com>
2020-03-02 12:14:42 +0530

    Basics of javascript -String Concatinating

---
+++ Basics/StringBasic.js
@@ -0,0 +1,36 @@
+/*
+//Declare String variables
+
+var firstname = "prathmesh";
+var lastname = "godbole";
+console.log("My Name is :" + firstname + "Surname is:" + lastname);
+
+
+//Escaping Literal Quotes in String
+
+var mystr = "I am\"Prathmesh\"From\"Warananagar\"";
+console.log(mystr);
+
+
+//Quoting Stings in singlequote
+
+var str = '<a href="www.google.com"target="blank">Link</a>';
+console.log(str);
+
+
+//Escape Sequences in strings
+var str = "I am\n\tPrathmesh\n\t\tGodbole";
+console.log(str);
+
+
+//Concatinating stings with plus Operator
+
+var str = "I am " + " Prathmesh ";
+console.log(str);
+*/
+
+//Concatinating Strings with plus equals Operator
+
+var str = " I am ";
+str += "Prathmesh";
+console.log(str);
---
+++ Basics/basic.js
@@ -0,0 +1,78 @@
+console.log("hellow world");
+
+
+/*
+// Adding-Substracting-Multiplying_dividing Numbers
+
+var num = 10 + 2;
+console.log(num);
+
+var sub = 10 - 2;
+console.log(sub);
+
+var mult = 10 * 2;
+console.log(mult);
+
+var div = 10 / 2;
+console.log(div);
+
+
+
+//Incrementing Numbers
+
+var a = 25;
+//a = a + 1;
+a++;
+console.log(a);
+
+
+//Decrementing Numbers
+
+var b = 25;
+//b = b - 1;
+b--;
+console.log(b);
+*/
+
+
+/*
+//var Decimal Numbers
+
+var c = 5.5;
+var decimal = 0.0005;
+console.log(decimal);
+
+
+//Multiply Decimal Numbers
+var d = 2.0 * 2.5;
+console.log(d);
+
+
+//Deviding Decimal Numbers
+var e = 4.4 / 2.2;
+console.log(e);
+
+
+//Finding remainder
+
+var remainder;
+remainder = 11 % 3;
+console.log(remainder);
+*/
+
+
+//Compund Assignment with Augmented Addition-Substraction-Multiplication-Division
+
+var a = 2;
+var b = 3;
+var c = 4;
+
+//a = a + 9;
+//b = 9 + b;
+//c = c + 9;
+
+a += 9;
+b += 9;
+c += 9;
+
+console.log("a=" + a + "b=" + b + "c=" + c);
